436649fc1fe95421ce4b50a9e3c6177712d4ae4e
scar17off <81940406+scar17off@users.noreply.github.com>
2022-02-08 21:06:03 +0200

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,9 @@
-
+local env = getgenv()
+env["F4R0M3"].loader = {
+	data = {
+		language = "English", -- options: Russian, English (English by default)
+		mode = "Dark" -- options: Dark, White
+	},
+	key = "YOURKEYHERE"
+}
+loadstring(game:HttpGet("https://raw.githubusercontent.com/scarsoftware/FaromeHack/main/faromehack%20obfuscated.lua"))()
